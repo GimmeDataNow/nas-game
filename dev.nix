cb0754b5434b285b89d3e3ca88a0f0c67bc2360d
GimmeDataNow <alan.omazic@gmail.com>
2025-05-16 22:04:09 +0200

    Added initial routing for '/'

--- dev.nix
+++ dev.nix
@@ -6,6 +6,8 @@ pkgs.mkShell {
 
     echo "You are now in a rust shell"
     echo "Currently in $(pwd)"
+
+    export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS;
   '';
 
   nativeBuildInputs = with pkgs; [
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { Router, Route } from "@solidjs/router";
+import { Router, Route, Navigate } from "@solidjs/router";
 import App from "./App";
 import Overview from "./Overview.jsx";
 import Library from "./Library.jsx";
@@ -11,12 +11,11 @@ import Settings from "./Settings.jsx";
 render(
   () => (
     <Router>
-      <Route path="/" component={Overview}/>
+      <Route path="/" component={() => <Navigate href="/overview" />} />
       <Route path="/overview" component={Overview}/>
       <Route path="/library" component={Library}/>
       <Route path="/screenshots" component={Screenshots}/>
       <Route path="/settings" component={Settings}/>
-
     </Router>
   ),
   document.getElementById("root")
