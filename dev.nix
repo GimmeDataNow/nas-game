f376fe1d42a443fd7ad00de40112f3c5c04f3f6b
GimmeDataNow <alan.omazic@gmail.com>
2025-05-16 11:53:44 +0200

    updated the nix flake for tauri development

--- dev.nix
+++ dev.nix
@@ -8,13 +8,26 @@ pkgs.mkShell {
     echo "Currently in $(pwd)"
   '';
 
-  nativeBuildInputs = with pkgs.buildPackages; [
-    rustc
-    cargo
-    rust-analyzer
-
-    # glibc.static
+  nativeBuildInputs = with pkgs; [
     pkg-config
+    gobject-introspection
+    cargo
+    cargo-tauri
+    nodejs
+  ];
+
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
     openssl
   ];
 }
